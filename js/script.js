baf6e56a99697a44d0368cd0d89d86308d0884c3
koles1ko <koles1ko@yandex.ru>
2022-01-21 17:36:21 +0300

    add-all

---
+++ js/script.js
@@ -0,0 +1,312 @@
+(function () {
+	window.addEventListener("DOMContentLoaded", () => {
+		const formBox = document.querySelector(".k1-form-slides")
+		const sendButton = formBox.querySelector(".slide-item__send")
+		const nextButton = formBox.querySelectorAll(".slide-item__next")
+		const finalButton = formBox.querySelector(".form-send-final")
+		
+		const errorEmptyField = "Это обязательный вопрос."
+		const errorInvalidField = "Неверно заполнено поле."
+		
+		let primarySocial = null
+		let currentSocial
+		
+		let formData = {}
+		
+		// function chooseCategory() {
+		// 	formBox.querySelectorAll(".slide-check-item").forEach(elem => {
+		// 		elem.addEventListener("click", function () {
+		// 			if (this.getAttribute("data-value") === "vk" ){
+		// 				formBox.querySelector(".slides__item--vk").classList.add("active")
+		// 				currentSocial = "slides__item--vk"
+		// 			} else {
+		// 				formBox.querySelector(".slides__item--fb").classList.add("active")
+		// 				currentSocial = "slides__item--fb"
+		// 			}
+		// 			formBox.querySelector(".slides__item--intro").classList.remove("active")
+		// 		})
+		// 	})
+		// }
+		
+		function putThanksMessage() {
+			const main =  document.querySelector(".main")
+			main.style.padding = "0"
+			main.innerHTML = `
+    <div class="thanks" >
+      Спасибо! Ваши результаты переданы, мы с Вами свяжемся
+    </div>
+  `
+			// document.write()
+		}
+		
+		// function setPrimary(e) {
+		// 	primarySocial = e.target.closest(".slide-check-item").querySelector("input").getAttribute("id")
+		// 	console.log(primarySocial)
+		// }
+		
+		function chooseSocial() {
+			let id
+			// let id = [...formBox.querySelectorAll("input[name=\"chose-social\"]")].filter(input => input.checked)[0].getAttribute("id")
+			formBox.querySelectorAll("input[name=\"chose-social\"]").forEach(elem => {
+				elem.addEventListener("change", function () {
+					// console.log("change")
+					id = this.getAttribute("id")
+					this.checked
+						? formBox.querySelector(`.social-item--${id}`).classList.add("chosen-soc")
+						: formBox.querySelector(`.social-item--${id}`).classList.remove("chosen-soc")
+					if (primarySocial === null) primarySocial = id
+					// formBox.querySelector(".slide-check__choose").classList.remove("error-soc")
+					!getChosenSocial().length
+						? formBox.querySelector(".slide-check__choose").classList.add("error-soc", "global-has-error")
+						: formBox.querySelector(".slide-check__choose").classList.remove("error-soc", "global-has-error")
+					// removeError(false, formBox.querySelector(".slide-check__choose"))
+					// if (this.checked) {
+					// 	formBox.querySelector(`.social-item--${id}`).classList.add("chosen-soc")
+					// } else {}
+					// id = [...formBox.querySelectorAll("input[name=\"chose-social\"]")].filter(input => input.checked)[0].getAttribute("id")
+					//
+					// formBox.querySelectorAll(".social-item").forEach(item => {
+					// 	item.classList.remove("chosen-soc")
+					//
+					// })
+					
+					// formBox.querySelector(`.social-item--${id}`).classList.add("chosen-soc")
+					
+				})
+				if (elem.checked) {
+					id = elem.getAttribute("id")
+					formBox.querySelector(`.social-item--${id}`).classList.add("chosen-soc")
+				}
+				// elem.addEventListener("change", setPrimary, {once: true})
+			})
+			// currentSocial = id
+		}
+		
+		function getChosenSocial() {
+			return [...formBox.querySelectorAll("input[name=\"chose-social\"]")].filter(input => input.checked)
+		}
+		
+		function setError(input, row, type) {
+			if (type === "socials" ) {
+				// console.log(row)
+				row.classList.add("error-soc", "global-has-error")
+				// console.log("no soc")
+				return false
+			}
+			if (type === "radio-skills" ) {
+				row.classList.add("error-skill", "global-has-error")
+				return false
+			}
+			const errorField = row.querySelector(".slide-item__error-field")
+			errorField.style.display = "block"
+			row.classList.add("global-has-error")
+			if (type === "email") {
+				!input.value || !input.value.length
+					? errorField.textContent = errorEmptyField
+					: errorField.textContent = errorInvalidField
+			}
+			if (type === "checkbox") {
+				errorField.textContent = errorEmptyField
+			}
+			if (type === "radio") {
+				errorField.textContent = errorEmptyField
+			}
+
+		}
+		
+		function removeError(input, row) {
+			row.classList.remove("global-has-error")
+			if (row.querySelector(".slide-item__error-field")) row.querySelector(".slide-item__error-field").style.display = "none"
+		}
+		
+		function checkError() {
+			// console.log(formBox.querySelectorAll(".global-has-error"))
+			return formBox.querySelector(".global-has-error")
+		}
+		
+		function scrollToError() {
+			checkError().scrollIntoView({behavior: 'smooth'});
+		}
+		
+		function validateInputText(input, row, type) {
+			
+			if (type === "email") {
+				const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
+				if (emailPattern.test(row.querySelector("input").value.toLowerCase())) {
+					if (row.classList.contains("global-has-error")) removeError(input, row)
+					return row.querySelector("input").value
+				} else {
+					setError(input, row, type)
+				}
+			}
+			if (type === "text") {
+				if (!row.querySelector("input").value) {
+					setError(false, row, "text")
+				} else {
+					removeError(false, row)
+					return row.querySelector("input").value
+				}
+			}
+			
+			return false
+		}
+		
+		function getFormDataSocial(currentSocial) {
+			
+			let data = {}
+			
+			formBox.querySelectorAll(`.social-item--${currentSocial} \[data-type]`).forEach(row => {
+				const typeName = row.getAttribute("data-type")
+				const arrFields = document.getElementsByName(typeName)
+				if (arrFields[0].getAttribute("type") === "checkbox" ) {
+					if ([...arrFields].filter(input => input.checked).length) {
+						data[typeName] = [...arrFields].filter(input => input.checked).map(elem => elem.getAttribute("id"))
+					} else {
+						setError(false, row, arrFields[0].getAttribute("type"))
+					}
+					
+				}
+				if (arrFields[0].getAttribute("type") === "radio") {
+					if ([...arrFields].filter(input => input.checked).length) {
+						data[typeName] = [...arrFields].filter(input => input.checked)[0].getAttribute("id")
+						
+					} else {
+						setError(false, row, arrFields[0].getAttribute("type"))
+					}
+				}
+				// if (typeName === "email") data[typeName] = validateInputText(false, row, typeName)
+				// if (typeName === "telegram-nickname") data[typeName] = validateInputText(false, row, "text")
+				
+			})
+			return data
+		}
+		
+		function getFormDataSkills() {
+			let data = {}
+			formBox.querySelectorAll(`.slides__item--skills [data-type]`).forEach(row => {
+				const typeName = row.getAttribute("data-type")
+				const arrFields = document.getElementsByName(typeName)
+				data[typeName] = {
+					title: row.querySelector(".slides-table__name").textContent.trim(),
+					// score: [...arrFields].filter(elem => elem.checked)[0].value
+				}
+				if ([...arrFields].filter(elem => elem.checked)[0]) {
+					data[typeName].score = [...arrFields].filter(elem => elem.checked)[0].value
+				} else {
+					setError(false, row, "radio-skills")
+				}
+			})
+			return data
+		}
+		
+		function getTelegramNickname() {
+			return formBox.querySelector("#user-telegram-nickname").value
+		}
+		
+		function getEmail() {
+			return formBox.querySelector("#user-email").value
+		}
+		
+		function getPhone() {
+			return formBox.querySelector("#user-phone").value
+		}
+		
+		function sendRequest(e) {
+			// e.preventDefault()
+			// validateInputText(false, formBox.querySelector("[data-type=\"email\"]"), "email")
+			validateInputText(false, formBox.querySelector("[data-type=\"telegram-nickname\"]"), "text")
+			const socials = getChosenSocial()
+			
+			if (!socials || !socials.length) {
+				setError(false, formBox.querySelector(".slide-check__choose"), "socials")
+				// return false
+			}
+			socials.forEach(elem => {
+				formData[`social_${elem.getAttribute("id")}`] = getFormDataSocial(elem.getAttribute("id"))
+			})
+			formData.skills = getFormDataSkills()
+			formData.currentSocial = currentSocial
+			formData.phone = getPhone()
+			formData.telegram = getTelegramNickname()
+			// const formData = Object.assign(getFormDataSocial(), getFormDataSkills() )
+			// return false
+			if (checkError()) {
+				// console.log(checkError())
+				scrollToError()
+				// console.log("check errors")
+				return false
+			}
+			let sendError = false, count = 0, score = 0
+			// return false
+			const url = location.origin + "/test.php"
+			// return false
+			fetch(url, {
+				method: "post",
+				body: JSON.stringify(formData),
+				headers: {
+					"fast-quiz": "new-action"
+				}
+			})
+				.then(data => data.text())
+				.then(test => {
+					console.log(test)
+					putThanksMessage()
+				})
+		}
+		
+		function nextForm() {
+			formData = getFormDataSocial()
+			if (checkError()) {
+				// console.log("ошибка")
+				return false
+			} else {
+				// console.log("ok")
+			}
+			formBox.querySelectorAll(".slides__item").forEach(elem => elem.classList.remove("active"))
+			formBox.querySelector(".slides__item--skills").classList.add("active")
+			// sendRequest()
+		}
+		
+		function init() {
+			formBox.querySelectorAll("input[type='text'], input[type='email']").forEach(input => {
+				input.addEventListener("blur", function() {
+					const type = this.getAttribute("type")
+					switch (type) {
+						case "email":
+						case "text":
+							validateInputText(this, this.closest(".slide-item__row"), type)
+							break
+						
+					}
+				})
+			})
+			formBox.querySelectorAll("input[type='checkbox']").forEach(input => {
+				input.addEventListener("change", function() {
+					if (input.getAttribute("name") === "chose-social") return false
+					if (![...document.getElementsByName(this.getAttribute("name"))].filter(input => input.checked).length) {
+						setError(false, input.closest(".slide-item__row"), "checkbox")
+					} else {
+						removeError(false, input.closest(".slide-item__row"))
+					}
+				})
+			})
+			formBox.querySelectorAll("input[type='radio']").forEach(input => {
+				input.addEventListener("change", function() {
+					if (input.closest(".slide-item__row")) removeError(false, input.closest(".slide-item__row"))
+					if (input.closest(".slides-table__row")) input.closest(".slides-table__row").classList.remove("error-skill", "global-has-error")
+					//
+				})
+			})
+			chooseSocial()
+			// chooseCategory()
+			// sendRequest()
+			// sendButton.addEventListener("click", sendRequest)
+			nextButton.forEach(button => button.addEventListener("click", nextForm))
+			finalButton.addEventListener("click", sendRequest)
+			
+		}
+		
+		init()
+		
+	})
+})()
---
+++ test.php
@@ -0,0 +1,472 @@
+<?php
+  if (!getallheaders()["fast-quiz"] || getallheaders()["fast-quiz"] !== "new-action") die();
+  $data = json_decode(file_get_contents("php://input"));
+  $skillsCount = 0;
+  $maxSkillsCount = count((array)$data->skills);
+  $skillsArr = array();
+//  $addresses = ["kd@ft10.ru"];
+  $addresses = ["az@ft10.ru", "support@ft10.ru","kd@ft10.ru"];
+//  echo "<pre>", print_r((array)$data->skills), "</pre>";
+//  echo "<pre>", var_dump(count((array)$data->skills)), "</pre>";
+  
+  function getResultMessage($percent) {
+    if ((int)$percent >= 90 ) return "Набран высокий балл, рекомендуем кандидата на должность";
+    if ((int)$percent >= 70 ) return "Рекомендуем рассмотреть кандидата на должность";
+    if ((int)$percent >= 60 ) return "Рекомендуем кандидата, но следует выполнить практическое задание";
+    return "Не рекомендуем";
+  }
+  
+  $message = "";
+//  $message .= "
+//    <tr>
+//    <td style='text-align: center; padding: 10px; border: #e9e9e9 1px solid;'>$messageTitle</td>
+//    </tr>
+//  ";
+//  $message .= "
+//    <tr>
+//    <td style='padding: 10px; border: #e9e9e9 1px solid;'>email: $data->email</td>
+//    </tr>
+//  ";
+  $message .= "
+    <tr>
+    <td style='padding: 10px; border: #e9e9e9 1px solid;'>Телефон: $data->phone</td>
+    </tr>
+  ";
+  $message .= "
+    <tr>
+    <td style='padding: 10px; border: #e9e9e9 1px solid;'>telegram: $data->telegram</td>
+    </tr>
+  ";
+  
+  
+  foreach($data->skills as $skill) {
+//    switch($skill->score) {
+//      case "low":
+//        break;
+//      case "lower":
+//        $skillsCount += 1;
+//        break;
+//      case "higher":
+//        $skillsCount += 2;
+//        break;
+//      case "high":
+//        $skillsCount += 3;
+//        break;
+//    }
+    if ($skill->score !== "low") {
+      $skillsArr[] = $skill;
+      $skillsCount += 1;
+    }
+  }
+  
+  echo "<pre>", print_r($skillsArr, true), "</pre>";
+  echo "<pre>", print_r($skillsCount, true), "</pre>";
+  
+  $answers = array(
+    "answers_vk" => array(
+      "pay-models" => array(
+        "value" => ["pay-models-clicks", "pay-models-shows", "pay-models-auto"],
+        "score" => 2
+      ),
+      "minimal-volume" => array(
+        "value" => "minimal-volume-100",
+        "score" => 1
+      ),
+      "minimal-distance" => array(
+        "value" => "minimal-distance-500",
+        "score" => 0
+      ),
+      "settings-save" => array(
+        "value" => ["settings-save-all"],
+        "score" => 1
+      ),
+      "max-peoples" => array(
+        "value" => "max-peoples-before-100",
+        "score" => 2
+      ),
+      "emoji" => array(
+        "value" => "emoji-12",
+        "score" => 1
+      ),
+      "minimal-cost" => array(
+        "value" => "minimal-cost-30",
+        "score" => 1
+      ),
+      "settings-budget" => array(
+        "value" => "settings-budget-clicks-day",
+        "score" => 2
+      ),
+      "universal" => array(
+        "value" => ["universal-auction", "universal-click", "universal-ctr"],
+        "score" => 2
+      ),
+    ),
+    "answers_fb" => array(
+      "ad-format" => array(
+        "value" => "ad-format-individual",
+        "score" => 1
+      ),
+      "what-target" => array(
+        "value" => "what-target-scope",
+        "score" => 1
+      ),
+      "media-plan" => array(
+        "value" => "media-plan-by-result",
+        "score" => 1
+      ),
+      "ad-rus" => array(
+        "value" => ["ad-rus-top", "ad-rus-moscow", "ad-rus-travel"],
+        "score" => 2
+      ),
+      "pay-for-show" => array(
+        "value" => "pay-for-show-5",
+        "score" => 2
+      ),
+      "min-budget" => array(
+        "value" => "min-budget-500",
+        "score" => 3
+      ),
+      "max-interest" => array(
+        "value" => "max-interest-5",
+        "score" => 3
+      ),
+      "min-distance" => array(
+        "value" => "min-distance-1",
+        "score" => 3
+      ),
+      "training" => array(
+        "value" => "training-lid",
+        "score" => 1
+      ),
+      "facebook" => array(
+        "value" => "facebook-20%",
+        "score" => 1
+      ),
+      "success-projects" => array(
+        "value" => "",
+        "score" => 0
+      ),
+    
+    
+    )
+  );
+  $scoreVK = 0;
+  $scoreFB = 0;
+  $maxScoreVK = 0;
+  $maxScoreFB = 0;
+  $samePercentVK = 0;
+  $samePercentFB = 0;
+  $messageVK = "";
+  $messageFB = "";
+  $score = 0;
+  $socialMaxScore = 0;
+  $count = 0;
+  $currentAnswers = null;
+  $skills = array();
+  $c = false;
+//  if ($data->currentSocial === "fb")
+//    $currentAnswers = $answers["answers_fb"];
+//  if ($data->currentSocial === "vk")
+//    $currentAnswers = $answers["answers_vk"];
+//  if (!$currentAnswers) {
+//    echo "нет соц сети";
+//    die();
+//  }
+
+
+
+//  $message .= "
+//    " . (($c = !$c) ? '<tr>' : '<tr style="background-color: #f8f8f8;">') . "
+//				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>Почта кандидата</b></td>
+//				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$data->email</td>
+//			</tr>
+//    ";
+//  $message .= "
+//    " . (($c = !$c) ? '<tr>' : '<tr style="background-color: #f8f8f8;">') . "
+//				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>Ник в телеграм</b></td>
+//				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$data->(telegram-nickname)</td>
+//			</tr>
+//    ";
+//  echo "<pre>", print_r($currentAnswers), "</pre>";
+  if ($data->social_vk) {
+    foreach($answers["answers_vk"] as $key => $ans) {
+      if (is_array($ans["value"])) {
+        if (!array_diff($ans["value"], $data->social_vk->$key) && array_diff($ans["value"], $data->social_vk->$key) !== null) {
+//          $count++;
+          $scoreVK += (int)$ans["score"];
+        }
+      } else {
+        if ($ans["value"] === $data->social_vk->$key) {
+//          $count++;
+          $scoreVK += (int)$ans["score"];
+        }
+      }
+    $maxScoreVK += (int)$ans["score"];
+    }
+    $message .= "
+      <tr>
+        <td style='text-align: center; padding: 10px; border: #e9e9e9 1px solid;'>Пройден тест таргетолога ВК</td>
+      </tr>
+    ";
+    $a = round(0.7*($scoreVK*100/$maxScoreVK));
+    $b = round(0.3*($skillsCount*100/$maxSkillsCount));
+    $samePercentVK = $a + $b;
+    $messageVK = getResultMessage($samePercentVK);
+    if ($scoreVK > 6 && $skillsCount > 9) {
+      echo "Сдал";
+      
+//      echo "<pre>", print_r($samePercentVK, true), "</pre>";
+//      echo "<pre>", print_r("max VK ", true), print_r($maxScoreVK, true), "</pre>";
+//      echo "<pre>", print_r("current VK ", true), print_r($score, true), "</pre>";
+//      echo "<pre>", print_r("max skills ", true), print_r($maxSkillsCount, true), "</pre>";
+//      echo "<pre>", print_r("current skills ", true), print_r($skillsCount, true), "</pre>";
+//
+//      echo "<pre>", print_r("soc 0.7 ", true), print_r(round(0.7*($score*100/$socialMaxScore)), true), "</pre>";
+//      echo "<pre>", print_r("skills 0.3 ", true), print_r(round(0.3*($skillsCount*100/$maxSkillsCount)), true), "</pre>";
+//      echo "<pre>", print_r(round(0.7*($score*100/$socialMaxScore))), "</pre>";
+//      echo "<pre>", print_r(round(0.3*($skillsCount*100/$maxSkillsCount))), "</pre>";
+//      $a = round(0.7*($score*100/$socialMaxScore));
+//      $b = round(0.3*($skillsCount*100/$maxSkillsCount));
+//      $samePercent = round(0.7*($score*100/$socialMaxScore)) + round(0.3*($skillsCount*100/$maxSkillsCount));
+//      echo "<pre>", print_r("after sum ", true), print_r($samePercent, true), "</pre>";
+      $message .= "
+      " . '<tr>' . "
+          <td style='text-align: center; padding: 10px; border: #e9e9e9 1px solid;'><b>Набранный балл: $samePercentVK / 100</b></td>
+        </tr>
+      ";
+      $message .= "
+      <tr>
+        <td style='text-align: center; padding: 10px; border: #e9e9e9 1px solid;'>$messageVK</td>
+      </tr>
+    ";
+    } else {
+      echo "Не сдал";
+     
+      $message .= "
+      " . '<tr>' . "
+          <td style='text-align: center; padding: 10px; border: #e9e9e9 1px solid;'><b>Набранный балл: $samePercentVK / 100</b></td>
+        </tr>
+      ";
+      $message .= "
+        <tr>
+          <td style='text-align: center; padding: 10px; border: #e9e9e9 1px solid;'>Не рекомендуем данного кандидата</td>
+        </tr>
+      ";
+    }
+  }
+  
+  if ($data->social_fb) {
+    $successProjects = "";
+    foreach($answers["answers_fb"] as $key => $ans) {
+      if (is_array($ans["value"])) {
+        if (!array_diff($ans["value"], $data->social_fb->$key) && array_diff($ans["value"], $data->social_fb->$key) !== null) {
+//          $count++;
+          $scoreFB += (int)$ans["score"];
+        }
+      } else {
+        if ($ans["value"] === $data->social_fb->$key) {
+//          $count++;
+          $scoreFB += (int)$ans["score"];
+        }
+      }
+      if ($key === "success-projects") {
+        echo $data->social_fb->$key;
+        echo "<pre>", print_r(123213312), "</pre>";
+        ($data->social_fb->$key === "success-projects-no")
+        ? $successProjects = "Менеe 10 успешных проектов"
+        : $successProjects = "Более 10 успешных проектов";
+      }
+    $maxScoreFB += (int)$ans["score"];
+    }
+    $message .= "
+      <tr>
+        <td style='text-align: center; padding: 10px; border: #e9e9e9 1px solid;'>Пройден тест таргетолога ФБ</td>
+      </tr>
+    ";
+    $a = round(0.7*($scoreFB*100/$maxScoreFB));
+    $b = round(0.3*($skillsCount*100/$maxSkillsCount));
+    $samePercentFB = $a + $b;
+    if ($scoreFB > 9 && $skillsCount > 9) {
+      
+      echo "Сдал";
+      echo "<pre>", print_r($samePercentFB, true), "</pre>";
+      $messageFB = getResultMessage($samePercentFB);
+      $message .= "
+      " . '<tr>' . "
+          <td style='text-align: center; padding: 10px; border: #e9e9e9 1px solid;'><b>Набранный балл: $samePercentFB / 100</b></td>
+        </tr>
+      ";
+      $message .= "
+      " . '<tr>' . "
+          <td style='text-align: center; padding: 10px; border: #e9e9e9 1px solid;'><b>$successProjects</b></td>
+          
+        </tr>
+      ";
+      $message .= "
+      <tr>
+        <td style='text-align: center; padding: 10px; border: #e9e9e9 1px solid;'>$messageFB</td>
+      </tr>
+    ";
+    } else {
+      echo "Не сдал";
+      
+      $message .= "
+      " . '<tr>' . "
+          <td style='text-align: center; padding: 10px; border: #e9e9e9 1px solid;'><b>Набранный балл: $samePercentFB / 100</b></td>
+        </tr>
+      ";
+      $message .= "
+        <tr>
+          <td style='text-align: center; padding: 10px; border: #e9e9e9 1px solid;'>Не рекомендуем данного кандидата</td>
+        </tr>
+      ";
+    }
+  }
+  
+//  foreach($currentAnswers as $key => $ans) {
+//    if (is_array($ans["value"])) {
+//      if (!array_diff($ans["value"], $data->social->$key) && array_diff($ans["value"], $data->social->$key) !== null) {
+//        $count++;
+//        $score += (int)$ans["score"];
+//      }
+//    } else {
+//      if ($ans["value"] === $data->social->$key) {
+//        $count++;
+//        $score += (int)$ans["score"];
+//      }
+//    }
+//    $socialMaxScore += (int)$ans["score"];
+//  }
+//  $samePercent = 0;
+//  $a = null;
+//  $b = null;
+//  if ($data->currentSocial === "fb") {
+//    if ($score > 9 && $skillsCount > 9) {
+//      $a = round(0.7*($score*100/$socialMaxScore));
+//      $b = round(0.3*($skillsCount*100/$maxSkillsCount));
+//      $samePercent = round(0.7*($score*100/$socialMaxScore)) + round(0.3*($skillsCount*100/$maxSkillsCount));
+//      echo "Сдал";
+//    } else {
+//      echo "Не сдал";
+//    }
+//  }
+//  echo "<pre>", var_dump($skillsCount), "</pre>";
+//  if ($data->currentSocial === "vk") {
+//    echo "<pre>", print_r($maxSkillsCount), "</pre>";
+//    echo "<pre>", print_r($skillsCount), "</pre>";
+//    if ($score > 6 && $skillsCount > 9) {
+//      echo "<pre>", print_r("max VK ", true), print_r($socialMaxScore, true), "</pre>";
+//      echo "<pre>", print_r("current VK ", true), print_r($score, true), "</pre>";
+//      echo "<pre>", print_r("max skills ", true), print_r($maxSkillsCount, true), "</pre>";
+//      echo "<pre>", print_r("current skills ", true), print_r($skillsCount, true), "</pre>";
+//
+//      echo "<pre>", print_r("soc 0.7 ", true), print_r(round(0.7*($score*100/$socialMaxScore)), true), "</pre>";
+//      echo "<pre>", print_r("skills 0.3 ", true), print_r(round(0.3*($skillsCount*100/$maxSkillsCount)), true), "</pre>";
+//      echo "<pre>", print_r(round(0.7*($score*100/$socialMaxScore))), "</pre>";
+//      echo "<pre>", print_r(round(0.3*($skillsCount*100/$maxSkillsCount))), "</pre>";
+//      $a = round(0.7*($score*100/$socialMaxScore));
+//      $b = round(0.3*($skillsCount*100/$maxSkillsCount));
+//      $samePercent = round(0.7*($score*100/$socialMaxScore)) + round(0.3*($skillsCount*100/$maxSkillsCount));
+//      echo "<pre>", print_r("after sum ", true), print_r($samePercent, true), "</pre>";
+//      echo "Сдал";
+//    } else {
+//      echo "Не сдал";
+//    }
+//  }
+
+//  $messageTitle = "";
+//  if ($samePercent < 60 ) $messageTitle = "Не рекомендуем";
+//  if ($samePercent >= 60 ) $messageTitle = "Рекомендуем, но кандидату следует выполнить практическое задание";
+//  if ($samePercent >= 70 ) $messageTitle = "Рекомендуем рассмотреть кандидата на должность";
+//  if ($samePercent >= 90 ) $messageTitle = "Набран высокий балл, рекомендуем кандидата на должность";
+
+//    $message .= "
+//    " . (($c = !$c) ? '<tr>' : '<tr style="background-color: #f8f8f8;">') . "
+//				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>max $data->currentSocial</b></td>
+//				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$socialMaxScore</td>
+//			</tr>
+//    ";
+//  $message .= "
+//    " . (($c = !$c) ? '<tr>' : '<tr style="background-color: #f8f8f8;">') . "
+//				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>current $data->currentSocial</b></td>
+//				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$score</td>
+//			</tr>
+//    ";
+//  $message .= "
+//    " . (($c = !$c) ? '<tr>' : '<tr style="background-color: #f8f8f8;">') . "
+//				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>max skills </b></td>
+//				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$maxSkillsCount</td>
+//			</tr>
+//    ";
+//  $message .= "
+//    " . (($c = !$c) ? '<tr>' : '<tr style="background-color: #f8f8f8;">') . "
+//				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>current skills</b></td>
+//				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$skillsCount</td>
+//			</tr>
+//    ";
+//  $message .= "
+//    " . (($c = !$c) ? '<tr>' : '<tr style="background-color: #f8f8f8;">') . "
+//				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>social 0.7</b></td>
+//				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$a</td>
+//			</tr>
+//    ";
+//  $message .= "
+//    " . (($c = !$c) ? '<tr>' : '<tr style="background-color: #f8f8f8;">') . "
+//				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>skills 0.3</b></td>
+//				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$b</td>
+//			</tr>
+//    ";
+//  $message .= "
+//    " . (($c = !$c) ? '<tr>' : '<tr style="background-color: #f8f8f8;">') . "
+//				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>summ</b></td>
+//				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$samePercent</td>
+//			</tr>
+//    ";
+  $message = "<table style='width: 300px'>$message</table>";
+  function adopt($text) {
+    return '=?UTF-8?B?' . Base64_encode($text) . '?=';
+  }
+  
+//  az@ft10.ru, support@ft10.ru
+  
+  $headers = "MIME-Version: 1.0" . PHP_EOL .
+    "Content-Type: text/html; charset=utf-8" . PHP_EOL .
+    'From: ' . adopt("test") . ' <' . "kd@ft10.ru" . '>' . PHP_EOL .
+    'Reply-To: ' . "kd@ft10.ru" . '' . PHP_EOL;
+//  mail("az@ft10.ru", "test", $message, $headers);
+//  mail("support@ft10.ru", "test", $message, $headers);
+foreach($addresses as $address){
+  mail($address, "test", $message, $headers);
+}
+
+//  0,7*fb/vk(% от общего кол-ва баллов)+0,3*компетенции(%от общего кол-ва баллов)
+//  =ЕСЛИ(C4>8;0*C7;ЕСЛИ(C5+C6<5;0*C7;ЕСЛИ(C4<5;ЕСЛИ(C5+C6>11;1*C7;0,5*C7);0,5*C7)))
+//    =(C10/23)*C11
+//  foreach($data->skills as $skill) {
+//    $skills[] = array(
+//      "title" => $skill->title,
+//      "score" => $skill->score,
+//    );
+//  }
+
+
+
+
+
+//  echo "<pre>", var_dump($count), "</pre>";
+//  echo "<pre>", print_r(2222), "</pre>";
+
+//  echo json_encode($answers);
+
+//  echo "<pre>", print_r(getallheaders()), "</pre>";
+
+
+
+
+
+
+
+
+
+
+
